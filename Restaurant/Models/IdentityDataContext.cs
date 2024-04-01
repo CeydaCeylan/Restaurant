10b0eed54baaa291d1eaad8e8ef09ea396ef7478
ceyla <ceyla@DESKTOP-F3QPEQJ>
2024-04-01 09:46:19 +0300

    Add project files.

---
+++ Restaurant/Models/IdentityDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant.Models
+{
+	public class IdentityDataContext : IdentityDbContext<AppUser, AppRole, string>
+	{
+		public IdentityDataContext(DbContextOptions<IdentityDataContext> options) : base(options)
+		{
+
+		}
+
+	}
+}
